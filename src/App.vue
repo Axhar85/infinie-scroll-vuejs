12b5028d2b8cfbcfca4d9575c4912ebcd3946b72
Axhar85 <azhararshad_ntu@yahoo.com>
2021-10-29 11:37:17 +0200

    added basic styling

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,51 @@
 <template>
   <div>
-    Hello
+    <header> 
+      <h1> My Anime Feed</h1>
+    </header>
+    <main> 
+      <!--Articales.. -->
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
 
 <style>
-#app {
+* {
+  margin: 0;
+  box-sizing: border-box;
+}
+body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  
+  background-color: #2c3e50;
+  color: #fff;
+  min-height: 100vh;
+  padding-top: 3rem;
+}
+
+header{
+  margin-bottom: 2rem;
+}
+
+header h1{
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+main{
+  padding: 0 2rem;
+  max-width: 640px;
+  margin: 0 auto;
 }
 </style>
