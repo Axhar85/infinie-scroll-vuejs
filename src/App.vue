cab6f509ef688404d1425841f2ecfa490048e303
Axhar85 <azhararshad_ntu@yahoo.com>
2021-11-02 11:16:51 +0100

    creating mount methood

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <h1> My Anime Feed</h1>
     </header>
     <main> 
-      <Post />
+      <Post  v-for="(anime, i) in anime_list" :key="i" :anime="anime" />
     </main>
   </div>
 </template>
@@ -49,8 +49,12 @@ export default {
               "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         });
       }
-    }
-  }
+      return anime;
+    },
+  },
+    mounted () {
+      this.anime_list = this.getAnime();
+    },
 }
 </script>
 
